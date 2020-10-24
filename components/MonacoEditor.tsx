61959d956a8800d741bc698327553a64b9b37b73
Mikk Martin <mikk.martin@gmail.com>
2020-10-24 07:37:07 +0300

    props sizing

--- components/MonacoEditor.tsx
+++ components/MonacoEditor.tsx
@@ -1,32 +1,65 @@
 import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
+import initialInput from '../static/initialInput'
+import styled from 'styled-components'
+import { useMeasure } from 'react-use'
 const MonacoEditor = dynamic(import('react-monaco-editor'), { ssr: false })
+const initialCode = JSON.stringify(initialInput, null, 2)
 
 const Editor = () => {
-  const [state, setState] = useState({ code: '// type your code...' })
-  const [postBody, setPostBody] = useState('')
+  const [postBody, setPostBody] = useState(initialCode)
+  const [ref, { width, height }] = useMeasure()
+
+  const onWillMount = monaco => {
+    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+      validate: true,
+      schemas: [
+        {
+          fileMatch: ['*'],
+          schema: {
+            type: 'object',
+            properties: {
+              p1: {
+                enum: ['v1', 'v2'],
+              },
+            },
+          },
+        },
+      ],
+    })
+  }
+
+  const onDidMount = () => {
+    //@ts-ignore
+    window.MonacoEnvironment.getWorkerUrl = (_moduleId: string, label: string) => {
+      if (label === 'json') return '_next/static/json.worker.js'
+    }
+  }
 
   return (
-    <MonacoEditor
-      editorDidMount={() => {
-        // @ts-ignore
-        window.MonacoEnvironment.getWorkerUrl = (_moduleId: string, label: string) => {
-          if (label === 'json') return '_next/static/json.worker.js'
-        }
-      }}
-      width="800"
-      height="600"
-      language="markdown"
-      theme="vs-dark"
-      value={postBody}
-      options={{
-        minimap: {
-          enabled: false,
-        },
-      }}
-      onChange={setPostBody}
-    />
+    <Container ref={ref}>
+      <MonacoEditor
+        editorWillMount={onWillMount}
+        editorDidMount={onDidMount}
+        onChange={setPostBody}
+        width={width}
+        height={height}
+        language="json"
+        theme="vs-dark"
+        value={postBody}
+        options={{
+          minimap: {
+            enabled: false,
+          },
+        }}
+      />
+    </Container>
   )
 }
+
+const Container = styled.div`
+  width: 450px;
+  height: 100vh;
+`
 
 export default Editor
