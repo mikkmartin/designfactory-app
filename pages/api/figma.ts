c8e30757df2d5b759caef02d2e3866731bd034b8
Mikk Martin <mikk.martin@gmail.com>
2020-10-25 15:40:30 +0200

    more cleanup

--- pages/api/figma.ts
+++ pages/api/figma.ts
@@ -1,7 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiResponse } from 'next'
 import { getTemplate } from '../../data/figma'
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async (_, res: NextApiResponse) => {
   const template = await getTemplate('QFHu9LnnywkAKOdpuTZcgE')
   res.json(template)
 }
