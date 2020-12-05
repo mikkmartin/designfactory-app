b4d17ca3adf81fe35f2b26fc88b819b8b3de8138
Mikk Martin <mikk.martin@gmail.com>
2020-12-05 20:15:49 +0000

    cache control

--- pages/api/figma.ts
+++ pages/api/figma.ts
@@ -5,5 +5,6 @@ import { defaults } from '../../static/invoice'
 export default async (req, res: NextApiResponse) => {
   const templateID = req.query.template
   const template = await getTemplate(templateID || defaults.template)
+  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
   res.json(template)
 }
