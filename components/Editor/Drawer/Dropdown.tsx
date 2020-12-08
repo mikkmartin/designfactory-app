ff459a55df979f3246939ffe377c0bfcf297b1ca
Mikk Martin <mikk.martin@gmail.com>
2020-12-08 20:03:15 +0000

    dropdown styleing

--- components/Editor/Drawer/Dropdown.tsx
+++ components/Editor/Drawer/Dropdown.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
-import { useDrawer } from "./DrawerContext";
+import { useDrawer } from './DrawerContext'
+import styled from 'styled-components'
 
 export const Dropdown = () => {
   const { dropdownTarget, setDropdownTarget, removeTemplate, setPanel, selectedTemplate } = useDrawer()
@@ -17,20 +17,42 @@ export const Dropdown = () => {
   }
 
   return (
-    <Menu
+    <StyledMenu
+      elevation={0}
       anchorEl={dropdownTarget}
       open={Boolean(dropdownTarget)}
       onClose={handleCloseMenu}
     >
-      <MenuItem onClick={handleDuplicate}>Edit</MenuItem>
-      <MenuItem
+      <StyledItem onClick={handleDuplicate}>Edit</StyledItem>
+      <StyledItem
         disabled={Boolean(!selectedTemplate?.dateAdded)}
         onClick={() => {
           removeTemplate()
           setDropdownTarget(null)
         }}>
         Remove
-      </MenuItem>
-    </Menu>
+      </StyledItem>
+    </StyledMenu>
   )
 }
+
+const StyledMenu = styled(Menu)`
+  .MuiPaper-root {
+    background: transparent;
+    border: 1px solid rgba(255, 255, 255, 0.05);
+  }
+  .MuiList-root {
+    min-width: 100px;
+    background: #3D4148;
+  }
+  .MuiListItem-root.Mui-disabled {
+    opacity: 0.3 !important;
+  }
+`
+const StyledItem = styled(MenuItem)`
+  :hover {
+    background: rgba(255, 255, 255, 0.1) !important;
+  }
+  color: white !important;
+  font-family: inherit !important;
+`;
