37f0d88903b508c0d8b22132906711797a5086d7
Mikk Martin <mikk.martin@gmail.com>
2020-12-26 12:38:18 +0000

    alignItems autolayout

--- components/Pdf/Elements/Frame.tsx
+++ components/Pdf/Elements/Frame.tsx
@@ -2,8 +2,11 @@ import { View } from '@react-pdf/renderer'
 import { FC } from 'react'
 import { getLayout } from './common'
 import { ContainerProvider } from './ContainerContext'
-import { Frame as FrameType } from 'figma-js'
+import { Frame as FrameBase } from 'figma-js'
 import { renderElement } from "./renderElement";
+
+type AlignItems = "SPACE_BETWEEN" | "CENTER" | "MAX"
+type FrameType = FrameBase & { primaryAxisAlignItems: AlignItems }
 
 export const Frame: FC<{ node: FrameType }> = ({ node }) => {
   const layout = getLayout(node)
@@ -14,10 +17,24 @@ export const Frame: FC<{ node: FrameType }> = ({ node }) => {
       <View style={{
         ...layout,
         display: 'flex',
+        justifyContent: getAlignment(node.primaryAxisAlignItems),
         flexDirection
       }}>
         {node.children.map(renderElement)}
       </View>
     </ContainerProvider>
   )
+}
+
+const getAlignment = (align: AlignItems) => {
+  switch (align) {
+    case 'SPACE_BETWEEN':
+      return 'space-between'
+    case 'MAX':
+      return 'flex-end'
+    case 'CENTER':
+      return 'center'
+    default:
+      return 'flex-start'
+  }
 }
