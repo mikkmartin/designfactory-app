2700afada162eabb9725a147c794da4893ffffbd
Mikk Martin <mikk.martin@gmail.com>
2020-12-26 10:15:53 +0000

    autolayout

--- components/Pdf/Elements/Rect.tsx
+++ components/Pdf/Elements/Rect.tsx
@@ -2,26 +2,30 @@ import { Canvas } from '@react-pdf/renderer'
 import { FC } from 'react'
 import { getLayout } from './common'
 import { Rectangle } from 'figma-js'
-import { useContainer } from './ContainerContext'
 
 export const Rect: FC<{ node: Rectangle, i: number }> = ({ node, i }) => {
-  //const { absoluteBoundingBox: parentBox } = useContainer()
-  const style = getLayout(node)
+  const padding = node.strokeWeight
+  const layout = getLayout(node)
+  const style = {
+    ...layout,
+    width: node.size.x + padding * 2,
+    height: node.size.y + padding * 2,
+    marginLeft: -padding,
+    marginTop: -padding,
+    marginBottom: layout.marginBottom as number - padding
+  }
   const width = node.size.x
   const height = node.size.y
-
-  console.log(node.name)
-  console.log(style)
 
   return (
     <Canvas key={i} style={style} paint={painter => {
       node.fills.forEach(({ color, opacity }) => {
-        painter.rect(0, 0, width, height)
+        painter.rect(padding, padding, width, height)
           .fillOpacity(opacity)
           .fill([color.r * 255, color.g * 255, color.b * 255])
       })
       node.strokes.forEach(({ color, opacity }) => {
-        painter.rect(0, 0, width, height)
+        painter.rect(padding, padding, width, height)
           .lineWidth(node.strokeWeight)
           .strokeOpacity(opacity)
           .stroke([color.r * 255, color.g * 255, color.b * 255])
