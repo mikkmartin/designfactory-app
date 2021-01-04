a25e5b674412e76de39307d606829c9d154e54a2
Mikk Martin <mikk.martin@gmail.com>
2021-01-04 17:25:53 +0000

    figma api cleanup

--- data/figma.ts
+++ data/figma.ts
@@ -5,11 +5,3 @@ const client = Figma.Client({ personalAccessToken })
 
 export const getTemplate = async (id: string) =>
   client.file(id, { geometry: 'paths' }).then(res => res.data)
-
-export const getPages = async (id: string) => {
-  const canvas = (await client
-    .file(id, { geometry: 'paths' })
-    .then(({ data }) => data.document.children.find(n => n.type === 'CANVAS'))) as Figma.Canvas
-  //return canvas.children.find(n => n.type === 'FRAME') as Figma.Frame
-  return canvas.children
-}
--- pages/api/figma.ts
+++ pages/api/figma.ts
@@ -1,13 +1,10 @@
 import { NextApiResponse } from 'next'
-import { getTemplate, getPages } from '../../data/figma'
+import { getTemplate } from '../../data/figma'
 import { defaults } from '../../static/invoice'
 
 export default async (req, res: NextApiResponse) => {
   const templateID = req.query.template
-  const allPages = req.query.pages
-  const template = allPages
-    ? await getPages(templateID || defaults.template)
-    : await getTemplate(templateID || defaults.template)
+  const template = await getTemplate(templateID || defaults.template)
   res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
   res.json(template)
 }
