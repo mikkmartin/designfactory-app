2bb379b568e496fdadfbf55f919170a47e8724d9
Mikk Martin <mikk.martin@gmail.com>
2021-01-08 06:54:52 +0000

    package upgrade fix

--- next.config.js
+++ next.config.js
@@ -1,32 +1,23 @@
 const withCSS = require('@zeit/next-css')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
-const withTM = require('next-transpile-modules')(['monaco-editor'])
 
-module.exports = withCSS(
-  withTM({
-    async rewrites() {
-      return [
-        {
-          source: '/invoice/:slug',
-          destination: '/api/invoice',
-        },
-      ]
-    },
-    webpack: config => {
-      const rule = config.module.rules
-        .find(rule => rule.oneOf)
-        .oneOf.find(r => r.issuer && r.issuer.include && r.issuer.include.includes('_app'))
-      if (rule) {
-        rule.issuer.include = [rule.issuer.include, /[\\/]node_modules[\\/]monaco-editor[\\/]/]
-      }
-      config.plugins.push(
-        new MonacoWebpackPlugin({
-          languages: ['json'],
-          filename: 'static/[name].worker.js',
-        })
-      )
-      return config
-    },
-    cssLoaderOptions: { url: false },
-  })
-)
+module.exports = withCSS({
+  async rewrites() {
+    return [
+      {
+        source: '/invoice/:slug',
+        destination: '/api/invoice',
+      },
+    ]
+  },
+  webpack: config => {
+    config.plugins.push(
+      new MonacoWebpackPlugin({
+        languages: ['json'],
+        filename: 'static/[name].worker.js',
+      })
+    )
+    return config
+  },
+  cssLoaderOptions: { url: false },
+})
