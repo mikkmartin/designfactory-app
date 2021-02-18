3e861c21ed15fadf71daf521ee02e43795aa941e
Mikk Martin <mikk.martin@gmail.com>
2021-02-18 07:14:57 +0000

    splitbee analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { GlobalStyles } from 'components/GlobalStyles'
+import Head from 'next/head'
 import * as Sentry from '@sentry/node'
 
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
@@ -11,6 +12,9 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
 export default function App({ Component, pageProps, err }) {
   return (
     <>
+      <Head>
+        <script async src="https://cdn.splitbee.io/sb.js" />
+      </Head>
       <GlobalStyles />
       <Component {...pageProps} err={err} />
     </>
