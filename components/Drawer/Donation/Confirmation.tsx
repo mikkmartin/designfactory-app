407a0bf1e4813c53b34eb146f14a5b7be5a10e75
Mikk Martin <mikk.martin@gmail.com>
2021-02-18 08:43:35 +0000

    show donation amount

--- components/Drawer/Donation/Confirmation.tsx
+++ components/Drawer/Donation/Confirmation.tsx
@@ -1,11 +1,17 @@
 import { childAnimations } from '../Tab'
 import { Button } from 'components/Common/Button'
 import { LogoAnimation } from './LogoAnimation'
+import { useDonation } from './DonationContext'
 
-export const Confirmation = ({ onDone, message = 'Thank you' }) => {
+export const Confirmation = ({ onDone, message = 'Thank you.' }) => {
+  const { amount, paymentType } = useDonation()
+  const description =
+    paymentType === 'Monthly'
+      ? `Montly donation of ${amount}€ added.`
+      : `${amount}€ donated.`
   return (
     <>
-      <LogoAnimation message={message} />
+      <LogoAnimation message={`${description} ${message}`} />
       <Button {...childAnimations} highlight width="100%" onClick={onDone}>
         Done
       </Button>
