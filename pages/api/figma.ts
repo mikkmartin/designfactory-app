524a2b279d57d321ce7862639fe13ed6b8e7f8bf
Mikk Martin <mikk.martin@gmail.com>
2021-03-22 13:53:57 +0000

    conditional caching

--- pages/api/figma.ts
+++ pages/api/figma.ts
@@ -1,10 +1,12 @@
 import { NextApiResponse } from 'next'
 import { getTemplate } from 'data/figma'
 import { defaults } from 'static/invoice'
+import { defaultTemplates } from 'static/defaultTemplates'
 
 export default async (req, res: NextApiResponse) => {
-  const templateID = req.query.template
-  const template = await getTemplate(templateID || defaults.template)
-  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
+  const templateID = req.query.template || defaults.template
+  const template = await getTemplate(templateID)
+  const existingTempalte = defaultTemplates.map(({ template }) => template).includes(templateID)
+  if (existingTempalte) res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
   res.json(template)
 }
