a9913c8043130284a40baeb08ea1c8548d7e43b0
Mikk Martin <mikk.martin@gmail.com>
2021-07-10 22:19:40 +0300

    correct env

--- lib/awsConfig.ts
+++ lib/awsConfig.ts
@@ -2,10 +2,10 @@ import aws from 'aws-sdk'
 
 export const authenticateAWS = () =>
   aws.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS,
+    secretAccessKey: process.env.AWS_SECRET,
+    region: process.env.AWS_REG,
     signatureVersion: 'v4',
   })
 
-export const Bucket = process.env.AWS_BUCKET_NAME
+export const Bucket = process.env.AWS_BUCKET
