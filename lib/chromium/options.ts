ee509bfc890354fe4e6f79138c02550415e06a5a
Mikk Martin <mikk.martin@gmail.com>
2021-07-11 10:40:45 +0300

    disable options

--- lib/chromium/options.ts
+++ lib/chromium/options.ts
@@ -1,30 +1,66 @@
-import chrome from 'chrome-aws-lambda';
-const exePath = process.platform === 'win32'
-? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-: process.platform === 'linux'
-? '/usr/bin/google-chrome'
-: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+import chrome from 'chrome-aws-lambda'
+const exePath =
+  process.platform === 'win32'
+    ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+    : process.platform === 'linux'
+    ? '/usr/bin/google-chrome'
+    : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
 interface Options {
-    args: string[];
-    executablePath: string;
-    headless: boolean;
+  args: string[]
+  executablePath: string
+  headless: boolean
 }
 
+const args = [
+  ...chrome.args,
+  '--disable-background-networking',
+  '--disable-background-timer-throttling',
+  '--disable-backgrounding-occluded-windows',
+  '--disable-breakpad',
+  '--disable-client-side-phishing-detection',
+  '--disable-default-apps',
+  '--disable-dev-shm-usage',
+  '--disable-extensions',
+  '--disable-features=AudioServiceOutOfProcess',
+  '--disable-hang-monitor',
+  '--disable-ipc-flooding-protection',
+  '--disable-notifications',
+  '--disable-offer-store-unmasked-wallet-cards',
+  '--disable-popup-blocking',
+  '--disable-print-preview',
+  '--disable-prompt-on-repost',
+  '--disable-renderer-backgrounding',
+  '--disable-speech-api',
+  '--disable-sync',
+  '--hide-scrollbars',
+  '--ignore-gpu-blacklist',
+  '--metrics-recording-only',
+  '--mute-audio',
+  '--no-default-browser-check',
+  '--no-first-run',
+  '--no-pings',
+  '--no-sandbox',
+  '--no-zygote',
+  '--password-store=basic',
+  '--use-gl=swiftshader',
+  '--use-mock-keychain',
+]
+
 export async function getOptions(isDev: boolean) {
-    let options: Options;
-    if (isDev) {
-        options = {
-            args: [],
-            executablePath: exePath,
-            headless: true
-        };
-    } else {
-        options = {
-            args: chrome.args,
-            executablePath: await chrome.executablePath,
-            headless: chrome.headless,
-        };
+  let options: Options
+  if (isDev) {
+    options = {
+      args: [],
+      executablePath: exePath,
+      headless: true,
     }
-    return options;
+  } else {
+    options = {
+      args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+    }
+  }
+  return options
 }
