abf98260a0a0721ebc91d70f999d6857d5e1081a
Mikk Martin <mikk.martin@gmail.com>
2021-07-17 12:46:21 +0300

    some cleanup

--- components/Canvas/Canvas.tsx
+++ components/Canvas/Canvas.tsx
@@ -2,18 +2,20 @@ import { renderElement } from './renderElement'
 import { TemplateProvider } from './TemplateContext'
 import { Page } from './elemets'
 
-export const Canvas = ({ onDataUpdate = _ => {}, nodes, disabledFields, componentSets, editable = true, data }) => {
+export const Canvas = ({
+  onDataUpdate = _ => {},
+  nodes,
+  disabledFields,
+  componentSets,
+  editable = true,
+  data,
+}) => {
   if (!nodes) return null
   const fontFamilies = []
+  const props = { data, disabledFields, componentSets, onDataUpdate, fontFamilies, editable }
 
   return (
-    <TemplateProvider
-      data={data}
-      disabledFields={disabledFields}
-      componentSets={componentSets}
-      onDataUpdate={onDataUpdate}
-      fontFamilies={fontFamilies}
-      editable={editable}>
+    <TemplateProvider {...props}>
       <Page>{nodes.map(renderElement)}</Page>
     </TemplateProvider>
   )
