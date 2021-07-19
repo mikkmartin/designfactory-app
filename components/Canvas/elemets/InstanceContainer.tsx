6784f10bed37eeb8ef0a1057a9e21936337c7d55
Mikk Martin <mikk.martin@gmail.com>
2021-07-19 20:51:04 +0300

    version bump

--- components/Canvas/elemets/InstanceContainer.tsx
+++ components/Canvas/elemets/InstanceContainer.tsx
@@ -6,35 +6,38 @@ import { InstanceProvider } from './InstanceContext'
 export const InstanceContainer = ({ children, ...props }) => {
   const { data, onDataUpdate, componentSets } = useTemplate()
 
-  const renderFromArray = () => {
+  const renderFromArray = arr => {
+    const updateInstance = (val, i) => {
+      onDataUpdate({
+        [props.name]: arr.map((prevVal, _i) => {
+          if (_i === i) return val
+          else return prevVal
+        }),
+      })
+    }
+
     const componentSet = Object.values(componentSets).find(set => {
       return set.find(component => component.id === children[0].componentId)
     })
 
-    return data[props.name].map((str, i) => {
+    return arr.map((str, i) => {
       const component = children[i]
         ? componentSet.find(component => component.id === children[i].componentId)
         : componentSet[componentSet.length - 1]
 
       return (
-        <InstanceProvider
-          key={i}
-          data={str}
-          update={val =>
-            onDataUpdate({
-              [props.name]: data[props.name].map((prevVal, _i) => {
-                if (_i === i) return val
-                else return prevVal
-              }),
-            })
-          }>
+        <InstanceProvider key={i} data={str} update={v => updateInstance(v, i)}>
           {renderElement(component)}
         </InstanceProvider>
       )
     })
   }
 
-  const renderedChildren = data[props.name] ? renderFromArray() : children.map(renderElement)
+  console.log(data[props.name])
+
+  const renderedChildren = data[props.name]
+    ? renderFromArray(data[props.name])
+    : children.map(renderElement)
 
   return <Box {...props}>{renderedChildren}</Box>
 }
