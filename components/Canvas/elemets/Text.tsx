2dd220646efaa40c298f783e2fee5c3741fece7b
Mikk Martin <mikk.martin@gmail.com>
2021-07-25 12:12:09 +0300

    editing json

--- components/Canvas/elemets/Text.tsx
+++ components/Canvas/elemets/Text.tsx
@@ -6,77 +6,66 @@ import { useCanvas } from '../model/CanvasModel'
 import { useEditor } from 'components/Editor'
 import { onSnapshot } from 'mobx-state-tree'
 import { observer } from 'mobx-react-lite'
+import { TextNode } from '../parseTemplate'
 
-export const Text = observer<{ style: any; content: any; name: any }>(
-  ({ style, content, name }) => {
-    const { editable, disabledFields } = useCanvas()
-    const instance = useInstance()
-    const editorData = useEditor()
-    const { data } = editorData
+export const Text = observer<TextNode>(({ style, content, name }) => {
+  const { editable, disabledFields } = useCanvas()
+  //const instance = useInstance()
+  const editor = useEditor()
+  const { data, setText } = editor
+  const json = data.toJSON()
 
-    const isDisabled = disabledFields?.find(fieldName => fieldName === name)
-    const isEditable = editable && !isDisabled
+  const isDisabled = disabledFields?.find(fieldName => fieldName === name)
+  const isEditable = editable && !isDisabled
 
-    const fillText = (name: string): string => {
-      if (instance) {
-        if (typeof instance.data === 'string') return instance.data
-        /*
-        for (const [key, value] of Object.entries(instance.data)) {
-          if (name === key) return value.toString()
-        }
-        */
-      }
-      for (const [key, value] of Object.entries(data)) {
-        if (name === key) return value as string
-      }
-      return content
+  const fillText = (name: string): string => {
+    for (const [key, value] of Object.entries(data)) {
+      if (name === key && typeof value === 'string') return value as string
     }
-
-    if (!isEditable) return <p style={style}>{isDisabled ? content : fillText(name)}</p>
-
-    const editor = useTipTap({
-      extensions: [StarterKit],
-      content: fillText(name),
-      onUpdate() {
-        /*
-        const { content }: JSONContent = this.getJSON()
-        if (content) {
-          const value = content.reduce((str, val, i) => {
-            if (i !== 0) str += '\n'
-            if (val.content) str = str + val.content.map(v => v.text)
-            return str
-          }, '')
-          //if (instance) instance.update({ [name]: value })
-          //else onDataUpdate({ [name]: value })
-        }
-        */
-      },
-      onFocus() {
-        setTimeout(() => {
-          document.execCommand('selectAll', false, null)
-        }, 0)
-      },
-    })
-
-    useEffect(() => {
-      if (!editor) return
-      if (typeof data[name] !== 'string') return
-      let content: any = {
-        type: 'text',
-      }
-      if (instance) content.text = instance.data
-      else if (data[name]?.length) content.text = data[name]
-      editor.commands.setContent({
-        type: 'doc',
-        content: [
-          {
-            type: 'paragraph',
-            content: [content],
-          },
-        ],
-      })
-    }, [editor, data])
-
-    return <EditorContent style={style} editor={editor} />
+    return content
   }
-)
+
+  if (!isEditable) return <p style={style}>{isDisabled ? content : fillText(name)}</p>
+
+  const contentEditor = useTipTap({
+    extensions: [StarterKit],
+    content: fillText(name),
+    onUpdate() {
+      const { content }: JSONContent = this.getJSON()
+      if (content) {
+        const value = content.reduce((str, val, i) => {
+          if (i !== 0) str += '\n'
+          if (val.content) str = str + val.content.map(v => v.text)
+          return str
+        }, '')
+        //if (instance) instance.update({ [name]: value })
+        setText({ [name]: value })
+      }
+    },
+    onFocus() {
+      setTimeout(() => {
+        document.execCommand('selectAll', false, null)
+      }, 0)
+    },
+  })
+
+  useEffect(() => {
+    if (!contentEditor || typeof json[name] !== 'string') return
+    contentEditor.commands.setContent({
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [
+            {
+              type: 'text',
+              text: json[name],
+            },
+          ],
+        },
+      ],
+    })
+  }, [contentEditor, json])
+
+  return <EditorContent style={style} editor={contentEditor} />
+})
--- components/Editor/model/EditorModel.ts
+++ components/Editor/model/EditorModel.ts
@@ -1,10 +1,17 @@
 import { Instance, types } from 'mobx-state-tree'
 import { ISchema } from 'components/Canvas/parseTemplate/getSchema'
 
+const TreeNode = types.map(
+  types.union(
+    types.late(() => TreeNode),
+    types.frozen()
+  )
+)
+
 export const RootModel = types.model('RootModel', {
   fileName: types.string,
   loading: types.boolean,
-  data: types.maybe(types.frozen<{ [k: string]: string }>()),
+  data: TreeNode,
   template: types.frozen<{ [key: string]: any }>(),
   downloadUrl: types.optional(types.string, ''),
   schema: types.frozen<ISchema>(),
@@ -14,6 +21,10 @@ export const RootActions = RootModel.actions(self => ({
   setData: data => (self.data = data),
   setSchema: schema => (self.schema = schema),
   setLoading: (state: boolean) => (self.loading = state),
+  setText: (obj: { [key: string]: any }) => {
+    const key = Object.keys(obj)[0]
+    self.data = { ...self.data.toJSON(), [key]: obj[key] }
+  },
 }))
 
 export type IEditorData = Instance<typeof RootModel> & Instance<typeof RootActions>
