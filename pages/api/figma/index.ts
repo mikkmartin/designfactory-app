b88c1695897c79c2705623625c22e8f04a47cada
Mikk Martin <mikk.martin@gmail.com>
2021-09-27 09:13:57 +0300

    figma api caching

--- pages/api/figma/index.ts
+++ pages/api/figma/index.ts
@@ -1,14 +1,12 @@
 import { NextApiResponse } from 'next'
 import { getTemplate } from 'data/figma'
 import { defaults } from 'static/invoice'
-import { defaultTemplatesv2 } from 'static/defaultTemplates'
+//import { defaultTemplatesv2 } from 'static/defaultTemplates'
 
 export default async (req, res: NextApiResponse) => {
   const templateID = req.query.template || defaults.template
   const template = await getTemplate(templateID)
-  const existingTemplate = defaultTemplatesv2.map(({ id }) => id).includes(templateID)
-  if (existingTemplate) {
-    res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
-  }
+  //const existingTemplate = defaultTemplatesv2.map(({ id }) => id).includes(templateID)
+  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
   res.json(template)
 }
