ff70d0d7661e9bf462203652fbe638afef409851
Mikk Martin <mikk.martin@gmail.com>
2021-10-25 20:53:43 +0300

    adding to db

--- pages/api/files/[slug].ts
+++ pages/api/files/[slug].ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         template,
       }
       res.json(newFile)
-      db.addFile(newFile)
+      await db.addFile(newFile)
     }
   } catch (e) {
     res.statusCode = 500
