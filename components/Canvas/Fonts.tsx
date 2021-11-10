4e21ee68b6d86f1d957b16bbc87c023a990300cd
Mikk Martin <mikk.martin@gmail.com>
2021-11-10 13:42:17 +0200

    load google font variants

--- components/Canvas/Fonts.tsx
+++ components/Canvas/Fonts.tsx
@@ -1,20 +1,36 @@
 import { FC } from 'react'
 import Head from 'next/head'
 import type { IFont } from './parseTemplate/getFonts'
+import { fontFamily } from 'components/GlobalStyles'
+
+//Get the font url in a similar form: 'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,900&display=swap'
+const getUrl = (font: IFont): string => {
+  const family = font.family.replace(/\s/g, '+')
+  const italic = font.weights.some(({ italic }) => italic) && 'ital,'
+  const weights = font.weights
+    .map((weight): string => {
+      const hasItalic = Boolean(italic)
+      return `${hasItalic && (weight.italic ? '1,' : '0,')}${weight.weight}`
+    })
+    .join(';')
+  return `https://fonts.googleapis.com/css2?family=${family}:${italic}wght@${weights}&display=swap`
+}
 
 export const Fonts: FC<{ fonts: IFont[] }> = ({ fonts }) => {
+  if (fonts.length === 0) return null
+  fonts = fonts.map(font => ({
+    ...font,
+    weights: font.weights
+      .sort((a, b) => (a.weight >= b.weight ? 1 : -1))
+      .sort((a, b) => (a.italic === b.italic ? 0 : a.italic ? 1 : -1)),
+  }))
+
   return (
     <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       {fonts.map(font => (
-        <link
-          key={font.family}
-          href={`https://fonts.googleapis.com/css2?family=${font.family}:wght@${font.weights
-            .sort((a, b) => a - b)
-            .join(';')}&display=swap`}
-          rel="stylesheet"
-        />
+        <link key={font.family} href={getUrl(font)} rel="stylesheet" />
       ))}
     </Head>
   )
--- components/Canvas/parseTemplate/getFonts.ts
+++ components/Canvas/parseTemplate/getFonts.ts
@@ -3,27 +3,70 @@ import { findNodes } from './parseTemplate'
 
 export interface IFont {
   family: string
-  weights: number[]
+  weights: { weight: number; italic?: boolean }[]
+}
+
+const addWeight = (weights: IFont['weights'], weight: IFont['weights'][0]): IFont['weights'] => {
+  const index = weights.findIndex(w => w.weight === weight.weight && w.italic === weight.italic)
+  if (index === -1) {
+    weights.push(weight)
+  }
+  return weights
 }
 
 export const getFonts = (nodes: Node[]): IFont[] =>
-  findNodes('TEXT', nodes).reduce((all, node) => {
-    const existing = all.find(n => n.family === node.style.fontFamily)
-    if (!existing) {
-      return [
-        ...all,
-        {
-          family: node.style.fontFamily,
-          weights: [node.style.fontWeight],
-        },
-      ]
-    } else {
-      return [
-        ...all.filter(n => n.family !== existing.family),
-        {
-          family: existing.family,
-          weights: [...new Set([...existing.weights, node.style.fontWeight])],
-        },
-      ]
-    }
-  }, [])
+  findNodes('TEXT', nodes)
+    .reduce((all, node) => {
+      let existing = all.find(n => n.family === node.style.fontFamily)
+      if (!existing) {
+        const weight = node.style.fontWeight
+        let weights: IFont['weights'] = [{ weight }]
+        if (node.style.italic) weights[0].italic = true
+        all = [
+          ...all,
+          {
+            family: node.style.fontFamily,
+            weights,
+          },
+        ]
+      } else {
+        all = [
+          ...all.filter(n => n.family !== existing.family),
+          {
+            family: existing.family,
+            weights: addWeight(existing.weights, {
+              weight: node.style.fontWeight,
+              italic: node.style.italic,
+            }),
+          },
+        ]
+      }
+      const overRides = Object.values(node.styleOverrideTable)
+      overRides.forEach(overRide => {
+        existing = all.find(f => f.family === overRide.fontFamily)
+        if (!existing) {
+          const weight = overRide.fontWeight
+          let weights: IFont['weights'] = [{ weight }]
+          if (overRide.italic) weights[0].italic = true
+          all = [
+            ...all,
+            {
+              family: overRide.fontFamily,
+              weights,
+            },
+          ]
+        } else {
+          all = [
+            ...all.filter(f => f.family !== existing.family),
+            {
+              family: existing.family,
+              weights: addWeight(existing.weights, {
+                weight: overRide.fontWeight,
+                italic: overRide.italic,
+              }),
+            },
+          ]
+        }
+      })
+      return all
+    }, [])
