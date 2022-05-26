cec518a641291440e1dadb8148a615008ceb31a1
Mikk Martin <mikk.martin@gmail.com>
2022-05-26 22:36:18 +0300

    Update extension-templates.ts

--- pages/api/extension-templates.ts
+++ pages/api/extension-templates.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest as Req, NextApiResponse as Res } from 'next'
 
 export default function handler(req: Req, res: Res) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.json([])
 }
