3a6d28ab319af478d7985a07c8a55160353c5038
Mikk Martin <mikk.martin@gmail.com>
2022-05-29 18:41:21 +0300

    Update extension-templates.ts

--- pages/api/extension-templates.ts
+++ pages/api/extension-templates.ts
@@ -3,5 +3,17 @@ import { NextApiRequest as Req, NextApiResponse as Res } from 'next'
 export default function handler(_: Req, res: Res) {
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.json([])
+  res.json([
+    {
+      name: 'Levila',
+      slug: 'story-levila-0e15k',
+      urlpattern: '*://www.levila.ee/(tekstid|raadio|video)/*',
+      selectors: {
+        title: 'article h1',
+        image: "meta[property='og:image'] | attr('content')",
+        body: 'article p',
+        media: 'main nav:nth-child(1) | txt',
+      },
+    },
+  ])
 }
