3325e2c58dbb709c2065243d7c2c1a8889f91bcf
Mikk Martin <mikk.martin@gmail.com>
2022-06-26 16:22:37 +0300

    Update extension-templates.ts

--- pages/api/extension-templates.ts
+++ pages/api/extension-templates.ts
@@ -5,7 +5,18 @@ export default function handler(_: Req, res: Res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.json([
     {
-      name: 'Levila',
+      name: 'Levila uudised post',
+      slug: 'levila-news-post-en629',
+      urlpattern: '*://www.levila.ee/uudised/*',
+      selectors: {
+        title: 'article h1',
+        image: "meta[property='og:image'] | attr('content')",
+        body: 'article p',
+        date: 'time',
+      },
+    },
+    {
+      name: 'Levila Story',
       slug: 'story-levila-0e15k',
       urlpattern: '*://www.levila.ee/(tekstid|raadio|video)/*',
       selectors: {
