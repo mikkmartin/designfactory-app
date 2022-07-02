4d66ef783710426b5b33365e94f0ef048d67fb7a
Mikk Martin <mikk.martin@gmail.com>
2022-07-02 13:52:11 +0300

    Update getLayout.ts

--- components/Canvas/parseTemplate/getLayout.ts
+++ components/Canvas/parseTemplate/getLayout.ts
@@ -1,18 +1,21 @@
 import { CSSProperties } from 'react'
-import { Frame } from '@mikkmartin/figma-js'
+import { Frame, Instance } from '@mikkmartin/figma-js'
 import { BoxNode, ContainerNode } from './parseTemplate'
 
-export const getLayout = (node: BoxNode, parentNode = null): CSSProperties => {
+type LayoutType = 'CANVAS_CHILD' | 'STATIC' | 'LAYOUT_ITEM'
+
+export const getLayout = (node: BoxNode, parentNode?: ContainerNode): CSSProperties => {
   const layoutMode = getLayoutMode(parentNode)
 
   const { paddingLeft, paddingRight, paddingTop, paddingBottom } = node as Frame
   const props = {
-    ...(node.type === 'FRAME' && autoLayoutContainer(node)),
+    ...((node.type === 'FRAME' || node.type === 'INSTANCE') && autoLayoutContainer(node)),
     paddingLeft,
     paddingRight,
     paddingTop,
     paddingBottom,
   }
+
   switch (layoutMode) {
     case 'CANVAS_CHILD':
       return {
@@ -31,56 +34,35 @@ export const getLayout = (node: BoxNode, parentNode = null): CSSProperties => {
     case 'LAYOUT_ITEM':
       return {
         ...props,
-        ...getPosition(node, parentNode),
+        position: 'relative',
         ...getSize(node),
       }
   }
 }
 
-function getPosition(node, parentNode): CSSProperties {
-  return node.layoutPositioning ? staticLayout(node, parentNode) : { position: 'relative' }
-}
-
-const staticLayout = (node: BoxNode, parentNode: ContainerNode): CSSProperties => {
-  const top = node.relativeTransform[1][2]
-  const left = node.relativeTransform[0][2]
+const staticLayout = (node: BoxNode, parentNode: ContainerNode | undefined): CSSProperties => {
+  const top = node.absoluteBoundingBox.y - (parentNode?.absoluteBoundingBox.y || 0)
+  const left = node.absoluteBoundingBox.x - (parentNode?.absoluteBoundingBox.x || 0)
   const { height, width } = node.absoluteBoundingBox
+  //@ts-ignore
   const parentSize = parentNode.absoluteBoundingBox
   const { horizontal, vertical } = node.constraints
 
   let layout: CSSProperties = {
     position: 'absolute',
   }
-  const transform = { x: 0, y: 0 }
 
-  if (horizontal === 'RIGHT') {
-    layout.right = parentSize.width - width - left
-  } else if (horizontal === 'CENTER') {
-    layout.top = ((top + height / 2) / parentSize.height) * 100 + '%'
-    transform.x = -50
-  } else {
-    layout.left = left
-  }
+  if (horizontal === 'RIGHT') layout.right = parentSize.width - width - left
+  else layout.left = left
 
-  if (vertical === 'BOTTOM') {
-    layout.bottom = parentSize.height - height - top
-  } else if (vertical === 'CENTER') {
-    layout.left = ((left + width / 2) / parentSize.width) * 100 + '%'
-    transform.y = -50
-  } else {
-    layout.top = top
-  }
-
-  if (vertical === 'CENTER' || horizontal === 'CENTER') {
-    const { x, y } = transform
-    layout.transform = `translate(${x}%, ${y}%)`
-  }
+  if (vertical === 'BOTTOM') layout.bottom = parentSize.height - height - top
+  else layout.top = top
 
   return layout
 }
 
-const getSize = (node): CSSProperties => {
-  const { x: width, y: height } = node.size
+const getSize = (node: BoxNode): CSSProperties => {
+  const { width, height } = node.absoluteBoundingBox
 
   if (node.type === 'TEXT') {
     switch (node.style.textAutoResize) {
@@ -94,26 +76,36 @@ const getSize = (node): CSSProperties => {
   } else {
     let size: CSSProperties = {}
 
-    if (node.layoutMode === 'HORIZONTAL') {
-      if (node.primaryAxisSizingMode === 'FIXED') size.width = width
-      if (node.counterAxisSizingMode === 'FIXED') size.height = height
-    } else if (node.layoutMode === 'VERTICAL') {
-      if (node.primaryAxisSizingMode === 'FIXED') size.height = height
-      if (node.counterAxisSizingMode === 'FIXED') size.width = width
+    if (
+      //@ts-ignore
+      node.layoutMode === 'HORIZONTAL' &&
+      //@ts-ignore
+      node.primaryAxisSizingMode === 'FIXED'
+    ) {
+      size.width = width
+    } else if (
+      //@ts-ignore
+      node.layoutMode === 'VERTICAL' &&
+      //@ts-ignore
+      node.counterAxisSizingMode === 'FIXED'
+    ) {
+      size.width = width
+      size.height = height
     } else {
+      //@ts-ignore
       if (node.layoutMode !== 'HORIZONTAL') size.width = width
+      //@ts-ignore
       if (node.layoutMode !== 'VERTICAL') size.height = height
     }
+
     return size
   }
 }
 
-type LayoutType = 'CANVAS_CHILD' | 'STATIC' | 'LAYOUT_ITEM'
-
 const getLayoutMode = (parent): LayoutType =>
   !parent ? 'CANVAS_CHILD' : !parent.layoutMode ? 'STATIC' : 'LAYOUT_ITEM'
 
-const autoLayoutContainer = (node: Frame): CSSProperties => {
+const autoLayoutContainer = (node: Frame | Instance): CSSProperties => {
   return {
     display: 'flex',
     flexDirection: node.layoutMode === 'HORIZONTAL' ? 'row' : 'column',
