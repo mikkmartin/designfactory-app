d2d19e1cfc57d02da4aa42a9236cee55ec7fcbde
Mikk Martin <mikk.martin@gmail.com>
2022-07-07 14:57:14 +0300

    Update Instance.tsx

--- components/Canvas/elemets/Instance.tsx
+++ components/Canvas/elemets/Instance.tsx
@@ -7,40 +7,24 @@ import { observer } from 'mobx-react-lite'
 export const Instance: FC<InstanceNode & { listParent?: null | string; nthChild: number }> =
   observer(({ componentId, children, listParent, nthChild, ...props }) => {
     const { componentSets, inputData } = useCanvas()
-    const { name } = props
+    const { name, style } = props
 
-    const getComponent = (name: string) => {
-      const overrideKey = Object.keys(inputData).find(key => key === name)
-      const hasOverride = Boolean(overrideKey)
-      if (!hasOverride || componentSets.sets.length < 1) return <div {...props}>{children}</div>
+    const overrideKey = Object.keys(inputData).find(key => key === name)
+    const hasOverride = Boolean(overrideKey)
+    if (!hasOverride) return <div {...props}>{children}</div>
 
-      const set = componentSets.sets.find(set => set.find(id => id === componentId))
-      const componentSet = componentSets.components.filter(c => set.includes(c.id))
+    const set = componentSets.sets.find(set => set.find(id => id === componentId))
+    const componentSet = componentSets.components.filter(c => set.includes(c.id))
 
-      const hasMultipleTextChildren = false
-      const overrideKeys = componentSet.map(c => c.name.split('=')[0])
-      const hasSingularOveride = overrideKeys.every(v => v === overrideKeys[0])
-
-      //If there is a singular overide and override is a string, apply override
-      if (
-        hasSingularOveride &&
-        typeof inputData[overrideKey] === 'string' &&
-        !hasMultipleTextChildren
-      ) {
-        const component = componentSet.find(
-          c => c.name.split('=')[1] === inputData[overrideKey]
-        ) as any
-        if (component) return renderElement(component)
-      }
-
-      //Otherwise get the override from the object with the corresponding key
-      const component = componentSet.find(c => {
-        const name = c.name.split('=')[1]
-        return !!Object.values(inputData[overrideKey]).find(val => val === name)
-      }) as any
-      return component ? renderElement(component) : children
+    //If there is a singular overide and override is a string, apply override
+    const component = componentSet.find(c => c.name.split('=')[1] === inputData[overrideKey]) as any
+    if (component) {
+      const { top, right, bottom, left, position } = style
+      return renderElement({
+        ...component,
+        style: { ...component.style, top, right, bottom, left, position },
+      })
     }
 
-    if (listParent && inputData[listParent] && inputData[listParent].length <= nthChild) return null
-    return getComponent(name)
+    return <div {...props}>{children}</div>
   })
